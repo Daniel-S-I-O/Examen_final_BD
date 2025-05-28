8a8db0ebe0bc3c7462c607eedb6dfbe5bd7d49a8
DANIEL <est.dsinestrozao0201@uml.edu.ni>
2025-05-28 14:37:17 -0600

    5

--- config.js
+++ config.js
@@ -1,38 +1,45 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
-// Load env variables
 dotenv.config();
 
-// Create a pool to avoid the overhead of creating a new connection every time one is needed
-
-const config = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
-    ssl: { rejectUnauthorized: false },
-    max: 10, // maximum number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-    connectionTimeoutMillis: 2000, // time to wait before timing out when connecting a new client
-});
-
-// Validate the connection
+const config = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+      max: 10,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT),
+      ssl: false, // Desactiva SSL en local
+      max: 10,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
+    });
 
 config.connect((err, client, release) => {
-    if (err) {
-        console.error('ERROR: ', err);
-        switch (err.code) {
-            case 'ECONNREFUSED':
-                console.error('La conexión a la base de datos fue rechazada.');
-                break;
-            default:
-                console.error('Error de conexión desconocido.');
-        }
-    } else {
-        console.log('Conexión a la base de datos establecida.');
-        release(); // Release the client back to the pool
+  if (err) {
+    console.error('ERROR: ', err);
+    switch (err.code) {
+      case 'ECONNREFUSED':
+        console.error('La conexión a la base de datos fue rechazada.');
+        break;
+      case 'ENOTFOUND':
+        console.error('No se pudo encontrar el host de la base de datos.');
+        break;
+      default:
+        console.error('Error de conexión desconocido.');
     }
+  } else {
+    console.log('Conexión a la base de datos establecida.');
+    release();
+  }
 });
+
 export { config };
